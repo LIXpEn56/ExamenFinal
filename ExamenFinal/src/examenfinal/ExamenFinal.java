f3d1008d8c454191af9e37321826610963cdbf56
Sandra Lizette Sanchez Ramirez <129222040+LIXpEn56@users.noreply.github.com>
2023-05-29 13:40:10 -0600

    Add files via upload

---
+++ ExamenFinal/src/examenfinal/ExamenFinal.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examenfinal;
+
+
+/**
+ *
+ * @author sardr
+ */
+public class ExamenFinal {
+    
+    public static void Sandra69971(){
+       System.out.println("Author - Sandra Lizette Sanchez Ramirez - 69971 - al069971@uacam.mx");
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Sandra69971();
+        // TODO code application logic here
+    }
+    
+}
